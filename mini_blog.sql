661bd7ab969ce7c1eb7ed6e1792f8c228fadb19c
DaisukeMatsuura <mail@brightful.jp>
2020-12-15 08:54:03 +0900

    Fix SQL

--- mini_blog.sql
+++ mini_blog.sql
@@ -8,19 +8,18 @@ CREATE TABLE user(
 ) ENGINE = INNODB;
 
 CREATE TABLE following(
-     id INTEGER AUTO_INCREMENT,
-     user_id INTEGER,
-     following_id INTEGER,
-     PRIMARY KEY(user_id, following_id)
+    user_id INTEGER,
+    following_id INTEGER,
+    PRIMARY KEY(user_id, following_id)
 ) ENGINE = INNODB;
 
 CREATE TABLE status(
-     id INTEGER AUTO_INCREMENT,
-     user_id INTEGER NOT NULL ,
-     body VARCHAR(255),
-     created_at DATETIME,
-     PRIMARY KEY(id),
-     UNIQUE KEY user_id_index(user_id)
+    id INTEGER AUTO_INCREMENT,
+    user_id INTEGER NOT NULL ,
+    body VARCHAR(255),
+    created_at DATETIME,
+    PRIMARY KEY(id),
+    INDEX user_id_index(user_id)
 ) ENGINE = INNODB;
 
 ALTER TABLE following ADD FOREIGN KEY (user_id) REFERENCES user(id);
